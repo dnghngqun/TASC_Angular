d7a71f1ffa508f0f954afbb2c494048037fc463c
dnghngqun <danghongquanvtc1111@gmail.com>
2024-10-25 23:06:59 +0700

    update

--- batch-8/src/app/app.module.ts
+++ batch-8/src/app/app.module.ts
@@ -3,16 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { DepartmentComponent } from './pages/department/department.component';
+import { UserComponent } from './pages/user/user.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    UserComponent,
+    DepartmentComponent,
+    DashboardComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
